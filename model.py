116702293baa0556d3fdcffb0972fd2ad7472b4f
HippoX20 <hippolyte.pilchen@gmail.com>
2024-03-05 11:37:17 +0100

    cleaned

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
+from torchvision.models import resnet50, ResNet50_Weights
 from torchvision.models.segmentation import fcn_resnet50, FCN_ResNet50_Weights
 
 
@@ -294,7 +295,29 @@ class SegmentationCNN(nn.Module):
 
         return torch.sigmoid(out)
     
-                
+class SegModel(nn.Module):
+    def __init__(self):
+        super(SegModel, self).__init__()
+        self.model = resnet50(weights=ResNet50_Weights.DEFAULT)
+        self.model.fc = nn.Linear(2048, 1)
+        
+    def forward(self, x):
+        x = self.model(x)
+        return torch.sigmoid(x)
+
+if __name__ == "__main__":
+    model = SegModel()
+        
+    corro_seg = CorroSeg('data', 'y_train.csv', shuffle = True,
+                 batch_size = 64, valid_ratio = 0.1, transform_img=None,  
+                 transform_test=None, test_params={'batch_size': 1, 'shuffle': False})
+    train_loader, val_loader, test_loader = corro_seg.get_loaders()
+    
+    for i, (x, y, z) in tqdm(enumerate(train_loader)):
+        pred = model(x)
+        print(pred.shape)
+        
+        
 class Jacard_UNet(nn.Module):
     def __init__(self, pretrained=True):
         super(Jacard_UNet, self).__init__()
